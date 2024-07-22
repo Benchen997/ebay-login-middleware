8bf38de1399620a557bb43cb04d2b9588a7acf57
Ben Chen <ben.chen@newaim.com.au>
2024-07-22 16:25:45 +1000

    disabled next rule

--- src/app/authSuccess/page.tsx
+++ src/app/authSuccess/page.tsx
@@ -1,27 +1,32 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { Suspense, useEffect, useState } from 'react';
 import { useSearchParams } from 'next/navigation';
 import Loading from './_components/loading';
 
-export default function Page() {
-  const searchParams = useSearchParams();
+const AuthSuccess = () => {
   const [code, setCode] = useState<string | null>(null);
+  const [expireIn, setExpireIn] = useState<number | null>(null);
+  const searchParams = useSearchParams();
 
   useEffect(() => {
     const codeParam = searchParams.get('code');
+    const expireInParam = searchParams.get('expires_in');
     setCode(codeParam);
+    setExpireIn(Number(expireInParam));
   }, [searchParams]);
 
   return (
     <main className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="text-4xl font-mono">Successfully logged in with eBay</h1>
-      {code ? (
-        <code className="block mt-5 text-2xl bg-gray-200 rounded-md p-2">{code}</code>
-      ) : (
-        <Loading />
-      )}
+      <Suspense fallback={<Loading />}>
+        <code className="block mt-3 text-2xl p-3 rounded-md bg-gray-200">{code}</code>
+        <code className="block mt-3 text-2xl p-3 rounded-md bg-gray-200">{expireIn}</code>
+      </Suspense>
     </main>
   );
-}
+};
+
+export default AuthSuccess;
+
 
