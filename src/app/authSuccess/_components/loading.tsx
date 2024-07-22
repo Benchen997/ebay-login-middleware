d6f9ee0a2953f25aabceb461af84b7e3889ce812
Ben Chen <ben.chen@newaim.com.au>
2024-07-22 16:06:51 +1000

    add suspense for code use search param

---
+++ src/app/authSuccess/_components/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="">
+        <h1>code is loading</h1>
+    </div>
+  );
+}
--- src/app/authSuccess/page.tsx
+++ src/app/authSuccess/page.tsx
@@ -1,13 +1,17 @@
-'use client';
-import { useSearchParams } from 'next/navigation'
+"use client";
+import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
+import Loading from "./_components/loading";
 export default function Page() {
-    const searchParams = useSearchParams();
-    const code = searchParams.get('code');
+  const searchParams = useSearchParams();
+  const code = searchParams.get("code");
   return (
     <main className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="text-4xl font-mono">Successfully logged in with ebay</h1>
       <h2 className="block mt-3 text-2xl">the acess code is:</h2>
-      <pre className="text-xl bg-gray-300 p-10 rounded-md mt-5 ">{code}</pre>
+      <Suspense fallback={<Loading />}>
+        <code className="block mt-3 text-2xl">{code}</code>
+      </Suspense>
     </main>
   );
 }
