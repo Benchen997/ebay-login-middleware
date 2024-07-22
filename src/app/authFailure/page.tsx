669bc6fba82d3a84bfc9d6ae0daf590593441695
Ben Chen <ben.chen@newaim.com.au>
2024-07-22 11:26:36 +1000

    add three endopoints

---
+++ src/app/authFailure/page.tsx
@@ -0,0 +1,7 @@
+export default function AuthFailure() {
+  return (
+    <main className="flex items-center justify-center min-h-screen">
+      <h1 className="text-4xl font-mono">Authentication failed</h1>
+    </main>
+  );
+}
---
+++ src/app/privacy/page.tsx
@@ -0,0 +1,7 @@
+export default function Privacy() {
+  return (
+    <main className="flex items-center justify-center min-h-screen">
+      <h1 className="text-4xl font-mono">Privacy Policy</h1>
+    </main>
+  );
+}
