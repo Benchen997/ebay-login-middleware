5aed45b86c5bdf39284d6c1885fb615e47cb9234
Ben Chen <ben.chen@newaim.com.au>
2024-07-26 11:48:37 +1000

    add suspense back to app

--- src/app/authSuccess/page.tsx
+++ src/app/authSuccess/page.tsx
@@ -1,17 +1,15 @@
-
 import DisplayCode from "@/app/authSuccess/_components/DisplayCode";
+import { Suspense } from "react";
 
-export default function Page(){
-    // do not make page.tsx as client side, it will not work when build
-    // page must be SSR as static page
+export default function Page() {
+  // do not make page.tsx as client side, it will not work when build
+  // page must be SSR as static page
 
-    return (
-        <main className="flex flex-col items-center justify-center min-h-screen">
-            <DisplayCode/>
-        </main>
-    );
-};
-
-
-
-
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen">
+      <Suspense fallback={<div>Loading...</div>}>
+        <DisplayCode />
+      </Suspense>
+    </main>
+  );
+}
