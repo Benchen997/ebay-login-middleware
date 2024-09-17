901aa0edff516e3d06bfd7227064073f4f1814c4
Ben Chen <ben.chen@newaim.com.au>
2024-09-18 09:52:35 +1000

    update redirect url default setting to production

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Suspense } from "react";
 export default function Page() {
   // do not make page.tsx as client side, it will not work when build
   // page must be SSR as static page
-  const redirectUrl = process.env.NEXT_PUBLIC_WEB_URL || "http://localhost:5200";
+  const redirectUrl = process.env.NEXT_PUBLIC_WEB_URL || "http://airoxycategoryassistweb.k8sdev.az.intra.newaim.com.au";
 
   return (
     <main className="flex flex-col items-center justify-center min-h-screen">
